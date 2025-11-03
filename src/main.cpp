4a098a128af1e2473f89d037deb26d9eb87753a6
dylanbenzi <dylanbenzi@gmail.com>
2025-11-02 20:44:23 -0500

    add basic movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Window/Keyboard.hpp>
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Pacmen");
     window.setFramerateLimit(144);
+
+    sf::CircleShape pacman(25.0f);
+    pacman.setFillColor(sf::Color(255, 255, 0));
 
     while (window.isOpen())
     {
@@ -16,6 +21,25 @@ int main()
         }
 
         window.clear();
+
+        window.draw(pacman);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+            pacman.move({0, -5.0f});
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+            pacman.move({-5.0f, 0});
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+            pacman.move({0, 5.0f});
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+            pacman.move({5.0f, 0});
+        }
+
         window.display();
     }
 }
